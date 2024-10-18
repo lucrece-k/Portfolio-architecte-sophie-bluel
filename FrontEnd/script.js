7b879248aef33bd07750feee3a04815f1f3c9036
Lucrèce KOUAME <kouamelucrece@gmail.com>
2024-10-19 01:21:29 +0200

    update modal telechargement et method POST

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -233,15 +233,15 @@ function modalDeTelechargement() {
   <svg xmlns="http://www.w3.org/2000/svg" width="70" height="61" viewBox="0 0 70 61" fill="none">
     <path d="M60.5517 6.88793C61.7228 6.88793 62.681 7.84612 62.681 9.01724V51.5768L62.0156 50.7118L43.9165 27.2894C43.3176 26.5042 42.3727 26.0517 41.3879 26.0517C40.4031 26.0517 39.4715 26.5042 38.8594 27.2894L27.8136 41.5824L23.7546 35.8998C23.1557 35.0614 22.1975 34.569 21.1595 34.569C20.1214 34.569 19.1632 35.0614 18.5644 35.9131L7.91783 50.8183L7.31896 51.6434V51.6034V9.01724C7.31896 7.84612 8.27715 6.88793 9.44827 6.88793H60.5517ZM9.44827 0.5C4.75048 0.5 0.93103 4.31945 0.93103 9.01724V51.6034C0.93103 56.3012 4.75048 60.1207 9.44827 60.1207H60.5517C65.2495 60.1207 69.069 56.3012 69.069 51.6034V9.01724C69.069 4.31945 65.2495 0.5 60.5517 0.5H9.44827ZM20.0948 26.0517C20.9337 26.0517 21.7644 25.8865 22.5394 25.5655C23.3144 25.2444 24.0186 24.7739 24.6118 24.1807C25.2049 23.5876 25.6755 22.8834 25.9965 22.1083C26.3175 21.3333 26.4828 20.5027 26.4828 19.6638C26.4828 18.8249 26.3175 17.9943 25.9965 17.2192C25.6755 16.4442 25.2049 15.74 24.6118 15.1468C24.0186 14.5537 23.3144 14.0831 22.5394 13.7621C21.7644 13.4411 20.9337 13.2759 20.0948 13.2759C19.2559 13.2759 18.4253 13.4411 17.6503 13.7621C16.8752 14.0831 16.171 14.5537 15.5779 15.1468C14.9847 15.74 14.5142 16.4442 14.1931 17.2192C13.8721 17.9943 13.7069 18.8249 13.7069 19.6638C13.7069 20.5027 13.8721 21.3333 14.1931 22.1083C14.5142 22.8834 14.9847 23.5876 15.5779 24.1807C16.171 24.7739 16.8752 25.2444 17.6503 25.5655C18.4253 25.8865 19.2559 26.0517 20.0948 26.0517Z" fill="#B9C5CC"/>
   </svg>
-  <img id="nouveau-projet"/>
+  <img id="nouveau-projet" alt="Aperçut de l'image"/>
+  <label for="file">+ Ajouter photo</label>
   <input type="file" id="file" name="file">
- <button onclick="file.click()" id="fileButton" class="button-ajouter-photo">+ Ajouter photo</button>
   <p>jpg, png : 4mo max</p>
-</div>
+</div> 
 <div class="input-paragraphe">
-Titre<br>
+<label for="titre">Titre</label>
 <input type="text" name="titre" id="titre" />
-Catégorie<br>
+<label for="categorie-select">Catégorie</label>
 <select name="categorie-select" id="categorie-select">
 </select>
 </div>
@@ -262,14 +262,16 @@ Catégorie<br>
     // Changer le background du button submit quand le titre est écrit
     function changerCouleurSubmit() {
       inputCategorie.addEventListener("focus", () => {
-        console.log(inputCategorie);
         submitPhoto.classList.remove("background-color-submit");
       });
     }
     changerCouleurSubmit();
     flecheRetour.addEventListener("click", () => {
-      baliseModalGallery.innerHTML = "";
-      createGalleryModal();
+      if (baliseModalGallery.children.length !== 0) {
+        baliseModalGallery.innerHTML = "";
+        createGalleryModal();
+      }
+      buttonAction.classList.remove("visibility-hidden");
       buttonAction.innerHTML = "Ajouter une photo";
       titreModal.innerHTML = "Galerie photo";
       flecheRetour.classList.add("visibility-hidden");
@@ -285,15 +287,15 @@ Catégorie<br>
 }
 function telechargerNouveauProjet() {
   const fileInput = document.getElementById("file");
-  const fileButton = document.getElementById("fileButton");
   const nouveauProjet = document.getElementById("nouveau-projet");
   const svgDeTelechargement = document.querySelector(
     ".contenu-ajout-photo svg"
   );
-  fileButton.addEventListener("click", (event) => {
-    event.preventDefault(); // Empêche la soumission par défaut
-    fileInput.click(); // Déclenche la sélection de fichier
-  });
+  const labelAjouterPhoto = document.querySelector(
+    ".contenu-ajout-photo label"
+  );
+  const paragrapheAjoutPhoto = document.querySelector(".contenu-ajout-photo p");
+
   // Affichage de l'image quand le fichier est sélectionné
   fileInput.addEventListener("change", (event) => {
     const file = fileInput.files[0];
@@ -304,12 +306,95 @@ function telechargerNouveauProjet() {
       reader.onload = (e) => {
         nouveauProjet.src = e.target.result;
         svgDeTelechargement.classList.add("display-none");
-        nouveauProjet.style.display = "block"; // Afficher l'image
+        nouveauProjet.style.display = "block";
+        labelAjouterPhoto.style.display = "none";
+        paragrapheAjoutPhoto.style.display = "none";
       };
-
-      reader.readAsDataURL(file); // Lire le fichier et générer une URL
+      // Lire le fichier et générer une URL
+      reader.readAsDataURL(file);
     }
   });
+  // function createNouveauProjet() {
+  //   const formAjoutPhoto = document.querySelector(".form-ajout-photo");
+  //   const formData = new FormData(formAjoutPhoto);
+  //   formAjoutPhoto.addEventListener("submit", (e) => {
+  //     e.preventDefault();
+  //     for (let pair of formData.entries()) {
+  //       console.log(`${pair[0]}: ${pair[1]}`);
+  //     }
+
+  //     // Envoyer le formdata a l'api
+  //     fetch(api + "works", {
+  //       method: "POST",
+  //       headers: { Authorization: `Bearer ${token}` },
+  //       body: formData,
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log("Réponse du serveur:", data);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Erreur lors de l'envoi du formulaire:", error);
+  //       });
+  //   });
+  // }
+
+  function createNouveauProjet() {
+    const formAjoutPhoto = document.querySelector(".form-ajout-photo");
+
+    formAjoutPhoto.addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const formData = new FormData();
+
+      // Récupérer les valeurs du formulaire
+      const titre = document.getElementById("titre");
+      const categorie = document.getElementById("categorie-select");
+      const fileInput = document.getElementById("file");
+
+      // Vérifier la présence des champs
+      if (!fileInput || !titre || !categorie) {
+        console.log("Tous les champs sont requis");
+        return;
+      }
+
+      // Ajouter les champs au FormData
+      formData.append("image", fileInput.files[0]); // Ajoute le fichier
+      formData.append("title", titre.value); // Ajoute le titre
+      formData.append("category", categorie.value); // Ajoute la catégorie
+
+      // Vérifier ce qui est ajouté à formData
+      for (let pair of formData.entries()) {
+        console.log(`${pair[0]}: ${pair[1]}`);
+      }
+
+      // Ajouter le token d'authentification et envoyer la requête à l'API
+      const token = localStorage.getItem("token"); // Assurez-vous d'avoir le bon token
+
+      fetch(api + "works", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: formData, // Envoie les données sous forme multipart
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Erreur lors de l'envoi : " + response.statusText);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Réponse du serveur:", data);
+        })
+
+        .catch((error) => {
+          console.error("Erreur lors de l'envoi du formulaire:", error);
+        });
+    });
+  }
+
+  createNouveauProjet();
 }
 
 // generer les option categorie dans la modal
